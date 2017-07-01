a10667064c5eee68ca1952aa69730f87375d1c66
Ricardo Cardoso <ricardo.cardoso@certponto.com.br>
2017-07-01 10:32:04 -0300

    Implemented aboutgame schene

---
+++ src/components/AboutGame.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class AboutGame extends Component {
+	render() {
+		return (
+			<Text style={{ flex: 1, backgroundColor: '#61BD8C' }}>
+				Está cena poderá ser adicionado informações do jogo.
+			</Text>
+		);
+	}
+}
