a41ddbe88ccd8e47b2c2a14b9232f6c4f43d4309
Ricardo Cardoso <ricardo.cardoso@certponto.com.br>
2017-07-01 10:19:56 -0300

    Implemented othersGames schne

---
+++ src/components/OthersGames.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class OthersGames extends Component {
+	render() {
+		return (
+			<Text style={{ flex: 1, backgroundColor: '#61BD8C' }}>
+				Está cena poderá ser adicionado informações de outros jogos do desenvolvedor.
+			</Text>
+		);
+	}
+}
